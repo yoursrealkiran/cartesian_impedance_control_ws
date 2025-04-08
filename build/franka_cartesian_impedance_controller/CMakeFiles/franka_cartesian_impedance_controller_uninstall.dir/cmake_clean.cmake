cec402a24d7d2e64e696cb557d8eead393ecc29c
Kiranraj Muthuraj <kiranrajofficial.de@yahoo.com>
2025-04-08 14:47:50 +0200

    chore: stop tracking ignored folders

--- build/franka_cartesian_impedance_controller/CMakeFiles/franka_cartesian_impedance_controller_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/franka_cartesian_impedance_controller_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/franka_cartesian_impedance_controller_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/franka_cartesian_impedance_controller/CMakeFiles/franka_cartesian_impedance_controller_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for franka_cartesian_impedance_controller_uninstall.
-# This may be replaced when dependencies are built.
